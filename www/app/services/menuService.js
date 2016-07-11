5133e90cf0a3920bb0be2665dc16d9cd075ed667
offlineprogrammer <mmalaka@gmail.com>
2016-07-11 14:08:25 +0100

    Beautify

--- www/app/services/menuService.js
+++ www/app/services/menuService.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     'use strict';
 
     angular
@@ -19,36 +19,36 @@
 
         var service = {};
 
-        service.isFirstLaunchComplete = function () {
+        service.isFirstLaunchComplete = function() {
             return cache.get(AllMENU_CACHE_KEY) !== undefined;
         };
 
 
-        service.getAllMenuData = function () {
+        service.getAllMenuData = function() {
             var deferred = $q.defer();
 
             var ref = new Firebase(appConfig.fireBaseURL);
-            ref.on("value", function (snapshot) {
+            ref.on("value", function(snapshot) {
                 // console.log(snapshot.val());
                 cache.put(AllMENU_CACHE_KEY, snapshot.val());
                 getCachedMenu();
                 deferred.resolve(true);
-            }, function (errorObject) {
+            }, function(errorObject) {
                 console.log("The read failed: " + errorObject.code);
             });
             return deferred.promise;
         };
 
 
-        service.getSubMenuItems = function (submenuId) {
-         
+        service.getSubMenuItems = function(submenuId) {
 
-             return _.compact(angular.copy(cache.get(AllMENU_CACHE_KEY)[submenuId]));
+
+            return _.compact(angular.copy(cache.get(AllMENU_CACHE_KEY)[submenuId]));
 
         };
 
 
-        service.menuSearch = function (searchText) {
+        service.menuSearch = function(searchText) {
             var deferred = $q.defer();
             var omenuItems = _.first(findByName(searchText, 0), 20);
             if (omenuItems === undefined || omenuItems.length === 0) {
@@ -66,13 +66,13 @@
             var searchTextLower = searchText.toLowerCase().trim();
 
             if (exactMatch) {
-                return angular.copy(_.find(cachedmenu, function (menuItem) {
+                return angular.copy(_.find(cachedmenu, function(menuItem) {
                     if (menuItem.name) {
                         return menuItem.name.toLowerCase() === searchTextLower;
                     }
                 }));
             } else {
-                return angular.copy(_.filter(cachedmenu, function (menuItem) {
+                return angular.copy(_.filter(cachedmenu, function(menuItem) {
                     if (menuItem.name) {
                         return menuItem.name.toLowerCase().indexOf(searchTextLower) >= 0;
                     }
@@ -80,71 +80,71 @@
             }
         }
 
-        service.getMenu = function () {
+        service.getMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).mainmenu) || [];
             return _.compact(angular.copy(jsonObject))
         };
 
-        service.getBurgerMenu = function () {
+        service.getBurgerMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).burgermenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getChikenBurgerMenu = function () {
+        service.getChikenBurgerMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).chikenburgermenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getSteakBurgerMenu = function () {
+        service.getSteakBurgerMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).steakburgermenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getOtherBurgerMenu = function () {
+        service.getOtherBurgerMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).otherburgermenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getStartersMenu = function () {
+        service.getStartersMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).startermenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getMealDealMenu = function () {
+        service.getMealDealMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).mealdealmenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getChikenDelightMenu = function () {
+        service.getChikenDelightMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).chikendelightmenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getKebabMenu = function () {
+        service.getKebabMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).kebabmenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getKidsDealMenu = function () {
+        service.getKidsDealMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).kidsmealsmenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getSoftDrinksMenu = function () {
+        service.getSoftDrinksMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).softdrinksmenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getSundriseMenu = function () {
+        service.getSundriseMenu = function() {
             var jsonObject = angular.fromJson(cache.get(AllMENU_CACHE_KEY).sundriesmenu) || [];
             return _.compact(angular.copy(jsonObject));
         };
 
-        service.getOrderItem = function (orderItemId) {
+        service.getOrderItem = function(orderItemId) {
 
             return cache.get(AllMENU_CACHE_KEY)[orderItemId];
 
-            
+
 
         };
 
@@ -179,4 +179,4 @@
 
 
     }
-} ());
+}());
