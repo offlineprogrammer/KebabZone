c4dab6ef729de9845c115a45012e70999ce52155
offlineprogrammer <mmalaka@gmail.com>
2016-07-19 22:03:33 +0100

    delivery charges

--- www/app/shoppingcart/shoppingCartController.js
+++ www/app/shoppingcart/shoppingCartController.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     'use strict';
 
     angular
@@ -9,49 +9,67 @@
         function init() {
             $scope.items = orderService.get();
             $scope.orderType = orderService.getOrderType();
+            $scope.deliveryCharges = 2;
             calculateTotal();
-       };
+        };
 
-        $scope.goBack = function () {
+        $scope.goBack = function() {
             $state.go('home');
         };
 
-         $scope.customer = {};
+        $scope.customer = {};
 
 
-        $scope.Order = function () {
-            orderService.placeOrders($scope.items, $scope.cartTotal,$scope.customer);
+        $scope.Order = function() {
+            orderService.placeOrders($scope.items, $scope.cartTotal, $scope.customer);
         };
 
 
-        $scope.updateQuantity = function (orderItem) {
+        $scope.updateQuantity = function(orderItem) {
             orderItem.quantity = Number(orderItem.quantity);
             orderService.updateOrderItemQuantity(orderItem);
             $scope.items = orderService.get();
-            
-             if ($scope.items.length === 0) {
-                  $state.go('home');
-             }
-            
+
+            if ($scope.items.length === 0) {
+                $state.go('home');
+            }
+
             calculateTotal();
         };
 
-         $scope.$on('orderService:orderplaced', function (event, count) {
+         $scope.updateTotal = function() {
+             if ($scope.orderType === 'deliveryOrder' ) {
+                $scope.cartTotal = $scope.cartTotal + $scope.deliveryCharges;
+            }
+
+
+           
+        };
+
+
+
+
+        $scope.$on('orderService:orderplaced', function(event, count) {
             $scope.cartCount = 0;
             $state.go('home');
         });
 
         function calculateTotal() {
-            
+
             var total = 0;
 
-            _.each($scope.items, function (item) {
+            _.each($scope.items, function(item) {
                 total += item.quantity * item.price;
             });
+
+            if ($scope.orderType === 'deliveryOrder' ) {
+                total = total + $scope.deliveryCharges;
+            }
+
 
             $scope.cartTotal = total;
         }
 
-    init();
+        init();
     }
-} ());
+}());
